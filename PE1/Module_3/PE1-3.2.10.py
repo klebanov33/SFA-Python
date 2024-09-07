1beeaf535dfe91a54499a7b5b18770e039dfe100
klebanov33 <31345284+klebanov33@users.noreply.github.com>
2024-09-07 19:31:36 +0300

    Add files via upload

---
+++ PE1/Module_3/PE1-3.2.10.py
@@ -0,0 +1,16 @@
+user_word = input("Enter a word: ") # Prompt the user to enter a word
+user_word = user_word.upper() # and assign it to the user_word variable.
+
+for letter in user_word:
+    if letter == "A":
+        continue
+    elif letter == "E":
+        continue
+    elif letter == "I":
+        continue
+    elif letter == "O":
+        continue
+    elif letter == "U":
+        continue
+    else:
+        print(letter)
